536df483205f5c57271ff99da3c2cd6b58cd4358
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-03-18 14:02:28 +0100

    Init Commit

---
+++ src/main/java/com/software/forecasting/EstimationApplication.java
@@ -0,0 +1,12 @@
+package com.software.forecasting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimationApplication.class, args);
+	}
+}
