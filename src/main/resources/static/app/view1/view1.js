474ebe292cbe57cfa7a760c4db6a71ed186f5a1e
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-03-25 17:32:29 +0100

    - Add highchart angular directive simple example

--- src/main/resources/static/app/view1/view1.js
+++ src/main/resources/static/app/view1/view1.js
@@ -1,14 +1,32 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular
+  .module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
 
-.controller('View1Ctrl', [function() {
+  .service('appService', appServiceInit)
 
-}]);
+  .controller('View1Ctrl', view1Controller);
+
+function appServiceInit($http) {
+  this.getBasicAreaChart = function () {
+    return $http({
+      "method": "get",
+      "url": 'data/basicAreaChart.json'
+    });
+  };
+}
+
+function view1Controller(appService, $scope) {
+  $scope.chartObj; // this will contain a reference to the highcharts' chart object
+  appService.getBasicAreaChart()
+    .then(function (chart) {
+      $scope.basicAreaChart = chart.data;
+    });
+}
