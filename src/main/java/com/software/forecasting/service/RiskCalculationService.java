e051131e9c6ec577dfff4017294f7b79ca6544f3
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-03-28 18:38:30 +0200

    - Change the name of SimulationResultBean to SimulationBean

--- src/main/java/com/software/forecasting/service/RiskCalculationService.java
+++ src/main/java/com/software/forecasting/service/RiskCalculationService.java
@@ -1,7 +1,7 @@
 package com.software.forecasting.service;
 
 import com.google.common.collect.Lists;
-import com.software.forecasting.model.SimulationResultBean;
+import com.software.forecasting.model.SimulationBean;
 
 import java.util.*;
 import java.util.stream.IntStream;
@@ -11,17 +11,17 @@ import java.util.stream.IntStream;
  */
 public class RiskCalculationService {
 
-  public List<SimulationResultBean> calculateRisk(List<Integer> efforts) {
-    List<SimulationResultBean> simulationPartitionList = createEffortPartitions(efforts);
-    List<SimulationResultBean> compressedSimulationList = mergeDuplicationEfforts(efforts);
+  public List<SimulationBean> calculateRisk(List<Integer> efforts) {
+    List<SimulationBean> simulationPartitionList = createEffortPartitions(efforts);
+    List<SimulationBean> compressedSimulationList = mergeDuplicationEfforts(efforts);
     return makeRiskEffort(compressedSimulationList, simulationPartitionList);
   }
 
-  private static List<SimulationResultBean> makeRiskEffort(List<SimulationResultBean> compressedSimulationList, List<SimulationResultBean> simulationPartitionList) {
+  private static List<SimulationBean> makeRiskEffort(List<SimulationBean> compressedSimulationList, List<SimulationBean> simulationPartitionList) {
     int index = 0;
-    List<SimulationResultBean> simulationPartitionReversedList = Lists.reverse(simulationPartitionList);
-    for (SimulationResultBean simPartition : simulationPartitionReversedList) {
-      for (SimulationResultBean simulation : compressedSimulationList) {
+    List<SimulationBean> simulationPartitionReversedList = Lists.reverse(simulationPartitionList);
+    for (SimulationBean simPartition : simulationPartitionReversedList) {
+      for (SimulationBean simulation : compressedSimulationList) {
         if (Objects.equals(simPartition.getTotal(), simulation.getTotal()) || index == 0) {
           simulation.setRisk(simPartition.getRisk());
           break;
@@ -34,23 +34,23 @@ public class RiskCalculationService {
     return compressedSimulationList;
   }
 
-  private List<SimulationResultBean> mergeDuplicationEfforts(List<Integer> efforts) {
+  private List<SimulationBean> mergeDuplicationEfforts(List<Integer> efforts) {
     List<Integer> removeEffortDuplication = new ArrayList<>(new LinkedHashSet(efforts));
-    List<SimulationResultBean> compressedSimulationList = new ArrayList<>();
-    removeEffortDuplication.forEach(effort -> compressedSimulationList.add(new SimulationResultBean(effort, Collections.frequency(efforts, effort), null))
+    List<SimulationBean> compressedSimulationList = new ArrayList<>();
+    removeEffortDuplication.forEach(effort -> compressedSimulationList.add(new SimulationBean(effort, Collections.frequency(efforts, effort), null))
     );
     return compressedSimulationList;
   }
 
-  private List<SimulationResultBean> createEffortPartitions(List<Integer> efforts) {
+  private List<SimulationBean> createEffortPartitions(List<Integer> efforts) {
     int partitionRisk = 20;
-    List<SimulationResultBean> simulationPartitionList = new ArrayList<>();
+    List<SimulationBean> simulationPartitionList = new ArrayList<>();
     IntStream.range(0, 10).forEach(
         i -> {
           int risk = i * 10;
           double index = (100.0 - risk) / 100 * partitionRisk;
           Integer effort = efforts.get((int) index - 1);
-          simulationPartitionList.add(new SimulationResultBean(effort, risk));
+          simulationPartitionList.add(new SimulationBean(effort, risk));
         });
     return simulationPartitionList;
   }
