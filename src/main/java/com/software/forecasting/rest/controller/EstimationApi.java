7a66e280a11bec5287806007d762a2d534772aa7
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-03-29 10:03:33 +0200

    - Clean Code optimization

--- src/main/java/com/software/forecasting/rest/controller/EstimationApi.java
+++ src/main/java/com/software/forecasting/rest/controller/EstimationApi.java
@@ -1,13 +1,10 @@
 package com.software.forecasting.rest.controller;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.google.common.base.Splitter;
 import com.googlecode.wickedcharts.highcharts.options.*;
 import com.software.forecasting.model.*;
 import com.software.forecasting.service.*;
 import com.software.forecasting.wrapper.DualAxesOptions;
-import jdk.nashorn.internal.runtime.JSType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,22 +17,22 @@ import java.util.*;
 @RestController
 public class EstimationApi {
 
-  private final ForecastingSimulationService forecastingSimulationService = new ForecastingSimulationService();
-  private final ForecastingCategorizationService forecastingCategorizationService = new ForecastingCategorizationService();
-  private final FileReaderService fileReaderService = new FileReaderService();
-  private final RiskCalculationService riskCalculationService = new RiskCalculationService();
-  private final ParameterParserService parameterParserService = new ParameterParserService();
-
+  private final ForecastingSimulationService forecastingSimulation = new ForecastingSimulationService();
+  private final ForecastingCategorizationService forecastingCategorization = new ForecastingCategorizationService();
+  private final FileReaderService fileReaderAdaptor = new FileReaderService();
+  private final RiskCalculationService riskCalculation = new RiskCalculationService();
+  private final ParameterParserService parameterParser = new ParameterParserService();
 
   @RequestMapping(value = "/estimationHighChart", method = RequestMethod.GET)
   @ResponseStatus(HttpStatus.FOUND)
   @JsonSerialize(include = JsonSerialize.Inclusion.ALWAYS)
   public Options getEstimationHighChart(@RequestParam(value = "futureTasks") String tasks, @RequestParam(value = "counts") Integer simulationLoops) throws IOException {
-    List<FutureTaskBean> futureTaskBeans = parameterParserService.parseParams(tasks);
-    List<HistoryDataBean> historicalData = fileReaderService.readHistoricalData();
-    List<FutureTaskBean> futureTasks = forecastingCategorizationService.categorise(futureTaskBeans, historicalData);
-    List<Integer> efforts = forecastingSimulationService.simulate(futureTasks, simulationLoops);
+    List<FutureTaskBean> futureTaskBeans = parameterParser.parseParams(tasks);
+    List<HistoryDataBean> historicalData = fileReaderAdaptor.readHistoricalData();
+    List<FutureTaskBean> futureTasks = forecastingCategorization.categorise(futureTaskBeans, historicalData);
+    List<Integer> efforts = forecastingSimulation.simulate(futureTasks, simulationLoops);
+    List<SimulationBean> simulationResultBeans = riskCalculation.calculateRisk(efforts, simulationLoops);
 
-    return new DualAxesOptions(riskCalculationService.calculateRisk(efforts, simulationLoops));
+    return new DualAxesOptions(simulationResultBeans);
   }
 }
